12b9e656c7d215f9acd495b46e7d10e36fd00820
ViktorTester <viktor345@inbox.lv>
2026-05-28 16:38:44 +0300

    feat(steps): added steps logic to one PO

--- src/ui/pages/ProductsPage.ts
+++ src/ui/pages/ProductsPage.ts
@@ -3,6 +3,7 @@ import {BasePage} from "@pages/BasePage";
 import {productsCopy as text} from "@ui/copy/productsCopy";
 import {Brand} from "@constants/brands";
 import {BrandsSection} from "@pages/components/Brands";
+import {step} from 'src/decorators/step'
 
 export class ProductsPage extends BasePage {
 
@@ -80,40 +81,55 @@ export class ProductsPage extends BasePage {
     }
 
     // Actions
+
+    @step('Serch for product: {0}')
     async searchForProduct(productName: string): Promise<void> {
         await this.searchInput.fill(productName);
         await this.searchBtn.click();
     }
 
+    @step('Check that the searched product title is visible')
+    async searchedProductsTitlePresent(): Promise<void> {
+        await expect(this.searchedProductsTitle).toHaveText(text.searchedProductsTitle);
+    }
+
+
+    @step('Click on the first product in the list')
     async clickFirstProduct(): Promise<void> {
         await this.firstProduct.click();
         await this.expectUrl('/product_details/1');
     }
 
+    @step('Increase product quantity to: {0}')
     async increaseProductQtyTo(qty: string): Promise<void> {
         await this.productQty.fill(qty);
         await expect(this.productQty).toHaveValue(qty);
     }
 
+    @step('Click on the "Add to cart" button')
     async addProductToCart(): Promise<void> {
         await this.addToCartBtn.click();
     }
 
+    @step('Click on the {0} brand')
     async selectBrand(brand: Brand): Promise<void> {
         await this.brands.selectBrand(brand)
     }
 
+    @step('Fill the review form with: {0}, {1}, {2}')
     async writeReview(name: string, email: string, message: string): Promise<void> {
         await this.reviewNameInput.fill(name);
         await this.reviewEmailInput.fill(email);
         await this.reviewTextInput.fill(message);
     }
 
+    @step('Click on the "Submit review" button')
     async submitReview(): Promise<void> {
         await this.submitReviewBtn.click();
     }
 
     // Assertions
+    @step('Check that the product data is visible')
     async checkProductInfo(): Promise<void> {
         await expect(this.productName).toBeVisible();
         await expect(this.productCategory).toBeVisible();
@@ -123,51 +139,57 @@ export class ProductsPage extends BasePage {
         await expect(this.productBrand).toBeVisible();
     }
 
-    async searchedProductsTitlePresent(): Promise<void> {
-        await expect(this.searchedProductsTitle).toHaveText(text.searchedProductsTitle);
-    }
-
+    @step('Check that the searched products count is equal to {0}')
     async checkProductsCount(count: number): Promise<void> {
         await expect(this.searchedProductCount).toHaveCount(count);
     }
 
+    @step('Check that the searched product name is equal to {0}')
     async checkSearchOutput(productName: RegExp): Promise<void> {
         await expect(this.searchResults).toContainText(productName);
     }
 
+    @step('Check that the page url is /products')
     async checkAllProductsSelected(): Promise<void> {
         await this.expectUrl('/products');
     }
 
+    @step('Check that the expected sections are visible')
     async expectedSections(): Promise<void> {
         await expect(this.centerTitle).toBeVisible();
         await expect(this.saleBanner).toBeVisible();
     }
 
+    @step('Check that the product count is equal to {0}')
     async allProductsPresent(count: number): Promise<void> {
         await expect(this.singleProduct).toHaveCount(count);
     }
 
+    @step('Check that the product is visible')
     async productIsVisible(): Promise<void> {
         await expect(this.singleProduct.first()).toBeVisible();
     }
 
+    @step('Check that the filter applied')
     async checkFilterApplied(categoryId: string): Promise<void> {
         await this.expectUrl('/category_products/' + categoryId);
         await expect(this.womenDressCategoryTitle).toBeVisible();
     }
 
+    @step('Check that the brands are filtered')
     async checkBrandsFiltering(expectedNames: string[]): Promise<void> {
         const actualNames = (await this.productNames.allTextContents());
 
         expect(actualNames.sort()).toEqual([...expectedNames].sort());
     }
 
+    @step('Check that the review title is visible')
     async checkReviewTitle(): Promise<void> {
         await expect(this.writeReviewTitle).toBeVisible();
 
     }
 
+    @step('Check that the successful review alert is present')
     async verifySuccessReviewAlert(): Promise<void> {
         await expect(this.successReviewAlertText).toBeVisible();
     }
